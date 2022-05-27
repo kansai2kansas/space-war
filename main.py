f389bf2a5809886a6578d35f6311ac5e5b4d2103
Thomas Andrikus <missing.samurai@gmail.com>
2022-05-26 20:42:49 -0400

    enemy and player ships have been created

--- main.py
+++ main.py
@@ -34,20 +34,63 @@ class Ship:
 		self.x = x
 		self.y = y
 		self.health = health
+		self.ship_img = None
 		self.laser_img = None
 		self.lasers = []
 		self.cool_down_counter = 0
+		
+		
+	def get_width(self):
+		return self.ship_img.get_width()
+
+	def get_height(self):
+		return self.ship_img.get_height()
 
 	def draw(self, window):
-		pygame.draw.rect
+		window.blit(self.ship_img, (self.x, self.y))
+		# pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 50, 50), 0)
+		# above is rectangle to be used as dummy object
+
+class Player(Ship):
+	def __init__(self, x, y, health = 100):
+		super().__init__(x, y, health)
+		self.ship_img = YELLOW_SPACE_SHIP
+		self.laser_img = YELLOW_LASER
+		self.mask = pygame.mask.from_surface(self.ship_img)
+		self.max_health = health
+
+class Enemy(Ship):
+	COLOR_MAP = {
+				"red": (RED_SPACE_SHIP, RED_LASER),
+				"green": (GREEN_SPACE_SHIP, GREEN_LASER),
+				"blue": (BLUE_SPACE_SHIP, BLUE_LASER)
+				}
+	def __init__(self, x, y, color, health = 100):
+		super().__init__(x, y, health)
+		self.ship_img, self.laser_img = self.COLOR_MAP[color]
+		self.mask = pygame.mask.from_surface(self.ship_img)
+
+	def move(self, vel):
+		self.y += vel
 
 
 def main():
 	run = True
 	FPS = 60 # frame per sec - the higher the number, the faster it is
-	level = 1
+	level = 0
 	lives = 5
 	main_font = pygame.font.SysFont("comicsans", 50)
+
+	enemies = []
+
+	enemy_vel = 1
+	wave_length = 5
+
+	player_vel = 5 # velocity of pixels moved every time a key is pressed
+
+	player = Player(300, 650)
+
+
 
 	clock = pygame.time.Clock()
 
@@ -60,15 +103,40 @@ def main():
 		WIN.blit(lives_label, (10, 10))
 		WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
+		for enemy in enemies:
+			enemy.draw(WIN)
 
+		player.draw(WIN)
 
 		pygame.display.update() #refreshes the display
 
 	while run:
 		clock.tick(FPS) # to ensure game stays consistent regardless of the device being used
-		redraw_window()
+		
+		if len(enemies)==0:
+			level += 1
+			wave_length += 5
+			for i in range(wave_length):
+				enemy = Enemy(random.randrange(50, WIDTH-100), random.randrange(-1500, -100), random.choice(["red", "green", "blue"]))
+				enemies.append(enemy)
+
+
 		for event in pygame.event.get(): # if event occurs, such as pressing a key, continue looping
 			if event.type == pygame.QUIT:
 				run = False
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_LEFT] and player.x - player_vel > 0:
+			player.x -= player_vel
+		if keys[pygame.K_RIGHT] and player.x + player_vel + player.get_width() < WIDTH:
+			player.x += player_vel
+		if keys[pygame.K_UP] and player.y - player_vel > 0:
+			player.y -= player_vel
+		if keys[pygame.K_DOWN] and player.y + player_vel + player.get_height() < HEIGHT:
+			player.y += player_vel
+
+		for enemy in enemies:
+			enemy.move(enemy_vel)
+
+		redraw_window()
 
 main()
